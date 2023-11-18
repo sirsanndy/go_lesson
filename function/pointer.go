da273f489d7e7c4901db2b00a39c60be4c4e7914
sirsanndy <sandy.hasanudin@yahoo.com>
2023-11-18 23:33:30 +0700

    Add pointer lesson

--- function/pointer.go
+++ function/pointer.go
@@ -1,1 +1,25 @@
 package main
+
+import "fmt"
+
+type Person struct {
+	Name    string
+	Age     int
+	Address string
+}
+
+func main() {
+	sandy := Person{
+		Name:    "Sandy",
+		Age:     29,
+		Address: "Bogor",
+	}
+
+	sandyPointer := &sandy
+	sandy2 := &sandy
+	sandyPointer.Name = "Sandy Pointer"
+
+	fmt.Println(sandy)
+	fmt.Println(sandyPointer)
+	fmt.Println(sandy2)
+}
