c5810c1e43ecaad3837c8204ba3875edd922abc2
sirsanndy <sandy.hasanudin@yahoo.com>
2023-11-19 11:59:12 +0700

    Create asterisk operator

---
+++ function/asterisk_opt.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name, Address string
+	Age           int16
+}
+
+func main() {
+	var person Person = Person{"Sandy", "Bogor", 29}
+	var person1 *Person = &person
+
+	person1.Address = "Batu"
+	person1 = &Person{"Ilham", "Yogyakarta", 19}
+	fmt.Println(person)
+	fmt.Println(person1)
+}
