d05c70ec833cc279578d4c2ab25a7b384306b626
sirsanndy <sandy.hasanudin@yahoo.com>
2023-11-21 11:21:14 +0700

    Fixing and enhance to recursive slice

--- function/func_slice.go
+++ function/func_slice.go
@@ -2,9 +2,17 @@ package main
 
 import "fmt"
 
+func sumAll(n int, numbers ...int) int {
+	if n <= 0 {
+		return 0
+	} else {
+		return sumAll(n-1, numbers...) + numbers[n-1]
+	}
+}
+
 func main() {
 	numbers := []int{10, 20, 30}
-	total := sumAll(numbers...)
+	total := sumAll(len(numbers), numbers...)
 
 	fmt.Println(total)
-}d
+}
