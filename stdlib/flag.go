983e6840502f6c3a9507ec42ba7eeb402b268807
sirsanndy <sandy.hasanudin@yahoo.com>
2023-11-23 20:29:19 +0700

    Add flag standard lib

--- stdlib/flag.go
+++ stdlib/flag.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 )
 
+func SayHello(firstName *string, lastName *string) {
+	fmt.Println("Hello", *firstName, *lastName)
+}
+
 func main() {
 	var firstName = flag.String("firstname", "firstname", "write your first name")
 	var lastName = flag.String("lastname", "lastname", "write your last name")
 
 	flag.Parse()
-	fmt.Println("Hello", *firstName, *lastName)
+	SayHello(firstName, lastName)
 }
