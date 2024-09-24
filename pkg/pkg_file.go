474e3a82b8d9044835f592cfd994399e9f2d569a
sirsanndy <ms.hasanudin@gmail.com>
2024-09-24 12:56:45 +0700

    Add read file function into package file implementation

--- pkg/pkg_file.go
+++ pkg/pkg_file.go
@@ -1,6 +1,11 @@
 package main
 
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 func createNewFile(name string, message string) error {
 	file, err := os.OpenFile(name, os.O_CREATE|os.O_WRONLY, 0666)
@@ -12,6 +17,29 @@ func createNewFile(name string, message string) error {
 	return nil
 }
 
+func readFile(name string) (string, error) {
+	file, err := os.OpenFile(name, os.O_RDONLY, 0666)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	var message string
+	for {
+		line, _, err := reader.ReadLine()
+		message += string(line)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return message, nil
+}
+
 func main() {
 	createNewFile("sample.log", "this is a sample log")
+	message, _ := readFile("sample.log")
+
+	fmt.Println(message)
 }
