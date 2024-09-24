b505818857b2c13f27a1445c9baa6e1b5eb80eec
sirsanndy <ms.hasanudin@gmail.com>
2024-09-24 12:52:03 +0700

    Add package file implementation

---
+++ pkg/pkg_file.go
@@ -0,0 +1,17 @@
+package main
+
+import "os"
+
+func createNewFile(name string, message string) error {
+	file, err := os.OpenFile(name, os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	file.WriteString(message)
+	return nil
+}
+
+func main() {
+	createNewFile("sample.log", "this is a sample log")
+}
