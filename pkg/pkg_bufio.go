79f0cbed6ad2c6e3a42af8468012256dcebf9010
sirsanndy <ms.hasanudin@gmail.com>
2024-09-24 12:34:28 +0700

    Add reader into bufio

--- pkg/pkg_bufio.go
+++ pkg/pkg_bufio.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 func main() {
+	var input = strings.NewReader("Hello!\nMy Name is Sandy")
+	var reader = bufio.NewReader(input)
+	for {
+		line, prefix, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(prefix, string(line))
+	}
+
 	var writer = bufio.NewWriter(os.Stdout)
 	_, _ = writer.WriteString("Hello World")
 	writer.Flush()
