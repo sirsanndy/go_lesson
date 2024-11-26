b1db36f7fd1e885c51f4e0488557fec02c76cf41
sandy <sandy@softwareseni.com>
2024-11-26 11:22:09 +0700

    Add error and fatal

--- unit_test/hello_world_test.go
+++ unit_test/hello_world_test.go
@@ -1,15 +1,22 @@
 package unit_test
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestSayHello(t *testing.T) {
 	name := "Sandy"
 	result := SayHello(name)
-	fmt.Println(result)
 	if result != "Hello "+name {
-		panic("Result should be 'Hello " + name + "'")
+		t.Error("SayHello doesn't give expected result 'Hello " + name + "'")
+	}
+}
+
+func TestSayHelloNegative(t *testing.T) {
+	name := "Sandy"
+	result := SayHello(name)
+
+	if result == "Hello "+"Eko" {
+		t.Fatal("SayHello doesn't give expected result 'Hello " + name + "'")
 	}
 }
