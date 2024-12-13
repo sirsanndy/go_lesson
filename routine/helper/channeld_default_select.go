03197a05e440d5e661a95e4bb01cf6dd494bee93
sandy <sandy@softwareseni.com>
2024-12-13 11:16:47 +0700

    Add channel default select

---
+++ routine/helper/channeld_default_select.go
@@ -0,0 +1,29 @@
+package helper
+
+import "fmt"
+
+func DefaultSelectChannel(channel1 chan string, channel2 chan string) {
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu data")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
