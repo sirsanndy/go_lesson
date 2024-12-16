db858dbef91d5697f7ed37e085ea1b2be293fafd
sandy <sandy@softwareseni.com>
2024-12-16 10:02:51 +0700

    Add unit test for pool

--- routine/pool_test.go
+++ routine/pool_test.go
@@ -1,1 +1,29 @@
 package routine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPool(t *testing.T) {
+	var pool = sync.Pool{
+		New: func() interface{} {
+			return "New"
+		},
+	}
+
+	pool.Put("Sandy")
+	pool.Put("Hasanudin")
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			data := pool.Get()
+			fmt.Println(data)
+			pool.Put(data)
+		}()
+	}
+	time.Sleep(3 * time.Second)
+	fmt.Println("Selesai")
+}
