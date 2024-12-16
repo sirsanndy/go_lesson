b7af32c3fe3dd74c4b21a3f8b28d16ec432239d7
sandy <sandy@softwareseni.com>
2024-12-16 10:11:21 +0700

    Change to wait group instead of time sleep

--- routine/pool_test.go
+++ routine/pool_test.go
@@ -8,7 +8,9 @@ import (
 )
 
 func TestPool(t *testing.T) {
+	group := &sync.WaitGroup{}
 	var pool = sync.Pool{
+		//For default value
 		New: func() interface{} {
 			return "New"
 		},
@@ -19,11 +21,14 @@ func TestPool(t *testing.T) {
 
 	for i := 0; i < 10; i++ {
 		go func() {
+			defer group.Done()
+			group.Add(1)
 			data := pool.Get()
 			fmt.Println(data)
+			time.Sleep(1 * time.Second)
 			pool.Put(data)
 		}()
 	}
-	time.Sleep(3 * time.Second)
+	group.Wait()
 	fmt.Println("Selesai")
 }
