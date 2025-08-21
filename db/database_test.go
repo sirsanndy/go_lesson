d5b8ad244a13e32f175878a2127899a5fcafcf42
sirsanndy <sandy.hasanudin@yahoo.com>
2025-08-22 00:06:38 +0700

    Add more test database

--- db/database_test.go
+++ db/database_test.go
@@ -3,7 +3,14 @@ package db
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
+)
+
+const (
+	errScanRowMsg       = "Failed to scan row: %v"
+	errExecSqlQueryMsg  = "Failed to execute SQL query: %v"
+	selectCustomerQuery = "SELECT id, name FROM customer"
 )
 
 func TestExecSql(t *testing.T) {
@@ -13,52 +20,138 @@ func TestExecSql(t *testing.T) {
 	ctx := context.Background()
 	script := `
 		-- Your SQL script here
-		INSERT INTO customer (user_name, email, name) VALUES ('sandy', 'sandy@go-lesson.com', 'Sandy');
+		INSERT INTO customer (user_name, email, name) VALUES ('hesa', 'hesa@go-lesson.com', 'Hesa');
 	`
 	_, err := db.ExecContext(ctx, script)
 	if err != nil {
 		t.Errorf("Failed to execute SQL script: %v", err)
 		panic(err)
 	}
-
 	fmt.Println("SQL script executed successfully")
 }
 
-func TestQuerySql(t *testing.T) {
+func TestQueryDefaultSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+	ctx := context.Background()
+	rows, err := db.QueryContext(ctx, selectCustomerQuery)
+	if err != nil {
+		t.Errorf(errExecSqlQueryMsg, err)
+		panic(err)
+	}
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			t.Errorf(errScanRowMsg, err)
+			return
+		}
+	}
+	rows.Close()
+	fmt.Println("Query executed successfully")
+}
+
+func TestQueryWGSql(t *testing.T) {
 	db := GetConnection()
 	defer db.Close()
 
 	ctx := context.Background()
-
-	script := "SELECT id, name FROM customer"
-	rows, err := db.QueryContext(ctx, script)
+	rows, err := db.QueryContext(ctx, selectCustomerQuery)
 	if err != nil {
-		t.Errorf("Failed to execute SQL script: %v", err)
+		t.Errorf(errExecSqlQueryMsg, err)
 		panic(err)
 	}
-	results := make(chan struct {
-		id, name string
-		err      error
-	})
+	// var wg sync.WaitGroup
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		// wg.Add(1)
+		go func(id, name string, err error) {
+			// defer wg.Done()
+			if err != nil {
+				t.Errorf(errScanRowMsg, err)
+				return
+			}
+		}(id, name, err)
+	}
+	// wg.Wait()
+	rows.Close()
+	fmt.Println("Query executed successfully")
+}
 
-	go func() {
+func BenchmarkQueryWGSql(b *testing.B) {
+	db := GetConnection()
+	defer db.Close()
+	ctx := context.Background()
+	rows, err := db.QueryContext(ctx, selectCustomerQuery)
+	if err != nil {
+		b.Errorf(errExecSqlQueryMsg, err)
+		panic(err)
+	}
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
 		for rows.Next() {
 			var id, name string
 			err := rows.Scan(&id, &name)
-			results <- struct {
-				id, name string
-				err      error
-			}{id, name, err}
+			wg.Add(1)
+			go func(id, name string, err error) {
+				defer wg.Done()
+				if err != nil {
+					b.Errorf(errScanRowMsg, err)
+					return
+				}
+			}(id, name, err)
 		}
-		close(results)
-	}()
-
-	for result := range results {
-		if result.err != nil {
-			t.Errorf("Failed to scan row: %v", result.err)
-			continue
-		}
-		fmt.Printf("ID: %s, Name: %s\n", result.id, result.name)
+		wg.Wait()
+		rows.Close()
 	}
-	defer rows.Close()
+}
+
+func BenchmarkQueryDefaultSql(b *testing.B) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	rows, err := db.QueryContext(ctx, selectCustomerQuery)
+	if err != nil {
+		b.Errorf(errExecSqlQueryMsg, err)
+		panic(err)
+	}
+	for i := 0; i < b.N; i++ {
+		for rows.Next() {
+			var id, name string
+			err := rows.Scan(&id, &name)
+			if err != nil {
+				b.Errorf(errScanRowMsg, err)
+				return
+			}
+			_ = id
+			_ = name
+		}
+		rows.Close()
+	}
+}
+
+func TestSqlSafe(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	name := "Sandy"
+	script := `SELECT id, name FROM customer WHERE name = $1`
+	rows, err := db.QueryContext(ctx, script, name)
+	if err != nil {
+		t.Errorf(errExecSqlQueryMsg, err)
+		panic(err)
+	}
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			t.Errorf(errScanRowMsg, err)
+			return
+		}
+	}
+	rows.Close()
+	fmt.Println("Query executed successfully")
 }
